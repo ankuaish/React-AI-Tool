cf63f06a514cb62ea7b05a16539a05572fd3c5c9
Aishwarya <anku.kesharwani17@gmail.com>
2025-09-19 19:30:47 +0530

    api

--- react-ai-tool/src/App.jsx
+++ react-ai-tool/src/App.jsx
@@ -1,9 +1,62 @@
+import { useState } from "react";
 import "./App.css";
+import { URL } from "./constant";
+import Answer from "./components/Answer";
 
 function App() {
+  const [question, setQuestion] = useState("");
+  const [result, setResult] = useState(undefined);
+
+  const payload = {
+    contents: [
+      {
+        parts: [{ text: question }],
+      },
+    ],
+  };
+
+  const askQuestion = async () => {
+    let response = await fetch(URL, {
+      method: "POST",
+      body: JSON.stringify(payload),
+    });
+
+    response = await response.json();
+    let dataString = response.candidates[0].content.parts[0].text;
+    dataString = dataString.split("* ");
+    dataString = dataString.map((item) => item.trim());
+    // console.log(dataString);
+    setResult(dataString);
+  };
   return (
     <>
-      <h1>React AI Tool</h1>
+      <div className="grid grid-cols-5 h-screen text-center">
+        <div className="col-span-1 bg-zinc-800"></div>
+        <div className="col-span-4 p-10">
+          <div className="container h-110 ">
+            <div className="text-white">
+              <ul>
+                {result &&
+                  result.map((item, index) => (
+                    <li className="text-left p-1">
+                      <Answer ans={item} key={index} />
+                    </li>
+                  ))}
+              </ul>
+            </div>
+          </div>
+          <div className="bg-zinc-800 w-1/2 p-1 pr-5 text-white m-auto rounded-4xl border border-zinc-700 flex h-16">
+            <input
+              type="text"
+              className="w-full h-full p-3 outline-none"
+              placeholder="Ask me anything"
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+            />
+            <button onClick={askQuestion}>Ask</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
---
+++ react-ai-tool/src/components/Answer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Answer = ({ ans, key }) => {
+  return <>{ans}</>;
+};
+
+export default Answer;
