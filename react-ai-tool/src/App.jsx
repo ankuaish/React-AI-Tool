63f641d275940871a6a66021dac5be420b02a525
Aishwarya Kesharwani <anku.kesharwani17@gmail.com>
2025-11-20 15:51:16 +0530

    gegegeg

--- react-ai-tool/src/App.jsx
+++ react-ai-tool/src/App.jsx
@@ -19,6 +19,9 @@ function App() {
   };
 
   const askQuestion = async () => {
+    if (!question) {
+      return false;
+    }
     if (localStorage.getItem("history")) {
       let history = JSON.parse(localStorage.getItem("history"));
       history = [question, ...history];
@@ -43,16 +46,47 @@ function App() {
       { type: "q", text: question },
       { type: "a", text: dataString },
     ]);
+    setQuestion("");
+  };
+
+  const clearHistory = () => {
+    localStorage.clear();
+    setRecentHistory([]);
+  };
+  const isEnter = (e) => {
+    if (e.key === "Enter") {
+      askQuestion();
+    }
   };
   return (
     <>
       <div className="grid grid-cols-5 h-screen text-center">
-        <div className="col-span-1 bg-zinc-800">
-          <ul>
+        <div className="col-span-1 bg-zinc-800 pt-3">
+          <h1 className="text-xl text-white flex justify-center">
+            <span>Recent Search</span>
+            <button className="pl-3 pt-1" onClick={clearHistory}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="20px"
+                viewBox="0 -960 960 960"
+                width="20px"
+                fill="#e3e3e3"
+              >
+                <path d="M312-144q-29.7 0-50.85-21.15Q240-186.3 240-216v-480h-48v-72h192v-48h192v48h192v72h-48v479.57Q720-186 698.85-165T648-144H312Zm336-552H312v480h336v-480ZM384-288h72v-336h-72v336Zm120 0h72v-336h-72v336ZM312-696v480-480Z" />
+              </svg>
+            </button>
+          </h1>
+
+          <ul className="text-left overflow-auto pt-2.5">
             {recentHistory &&
-              recentHistory.map((item) => {
-                <li>{item}</li>;
-              })}
+              recentHistory.map((item, index) => (
+                <li
+                  key={index}
+                  className="p-1 pl-5 truncate text-zinc-400 cursor-pointer hover:bg-zinc-700 hover:text-zinc-200"
+                >
+                  {item}
+                </li>
+              ))}
           </ul>
         </div>
         <div className="col-span-4 p-10">
@@ -116,6 +150,7 @@ function App() {
               placeholder="Ask me anything"
               value={question}
               onChange={(e) => setQuestion(e.target.value)}
+              onKeyDown={isEnter}
             />
             <button onClick={askQuestion}>Ask</button>
           </div>
