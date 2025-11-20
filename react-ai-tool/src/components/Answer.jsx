6758b3e059c042f1f357d6a314cd15e71908e465
Aishwarya Kesharwani <anku.kesharwani17@gmail.com>
2025-11-21 00:43:34 +0530

    hahah

--- react-ai-tool/src/components/Answer.jsx
+++ react-ai-tool/src/components/Answer.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { checkHeading, replaceHeadingStars } from "../helper";
-import SyntaxHighlighter from "react-syntax-highlighter";
+
 import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import ReactMarkdown from "react-markdown";
 
@@ -17,28 +17,37 @@ const Answer = ({ ans, indexProp, totalResult, type }) => {
   const renderer = {
     code({ inline, className, children, ...props }) {
       const match = /language-(\w+)/.exec(className || "");
-      return !inline && match ? (
-        <SyntaxHighlighter
-          {...props}
-          children={String(children).replace(/\n$/, "")}
-          language={match[1]}
-          style={dark}
-          preTag="div"
-        />
-      ) : (
+
+      if (!inline && match) {
+        return (
+          <SyntaxHighlighter
+            {...props}
+            language={match[1]}
+            style={dark}
+            PreTag="div"
+          >
+            {String(children).replace(/\n$/, "")}
+          </SyntaxHighlighter>
+        );
+      }
+
+      return (
         <code {...props} className={className}>
           {children}
         </code>
       );
     },
   };
-
   return (
     <>
       {indexProp == 0 && totalResult > 1 ? (
-        <span className="pt-2 text-xl block text-white">{answer}</span>
+        <span className="pt-2 text-xl block dark:text-white text-black">
+          {answer}
+        </span>
       ) : heading ? (
-        <span className="pt-2 text-lg block text-white">{answer}</span>
+        <span className="pt-2 text-lg block dark:text-white text-black">
+          {answer}
+        </span>
       ) : (
         <span className={type === "q" ? "pl-1" : null}>
           <ReactMarkdown components={renderer}>{answer}</ReactMarkdown>
