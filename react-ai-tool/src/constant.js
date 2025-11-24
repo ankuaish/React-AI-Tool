de10ec5f9b5f20550f6550628fb671777b98284d
Aishwarya Kesharwani <anku.kesharwani17@gmail.com>
2025-11-24 14:32:43 +0530

    Update constant.js

--- react-ai-tool/src/constant.js
+++ react-ai-tool/src/constant.js
@@ -1,2 +1,2 @@
 export const URL =
-  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDeYM6edjc_RbqzAURZZ4vmFt01c8E1S_c";
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?";
